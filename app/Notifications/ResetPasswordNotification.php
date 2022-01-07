91f3224d99ceefa238432aa27f3125a6452835dc
nils0n <nilson_jose.v@hotmail.com>
2022-01-07 20:54:08 -0300

    feat: reset email

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+    public $token, $email ,$name;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token, $email, $name)
+    {
+        $this->token = $token;
+        $this->email = $email;
+        $this->name = $name;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = url('/password/reset/' . $this->token . '?email=' . $this->email);
+        $minutos = config('auth.passwords.' . config('auth.defaults.passwords') . '.expire');
+
+        return (new MailMessage)
+            ->subject('Atualização de senha')
+            ->greeting('Olá, '.$this->name)
+            ->line('Você solicitou a recuperação de senha')
+            ->action('Clique aqui para atualizar a senha', $url)
+            ->line('O link acima expira em ' . $minutos . 'minutos')
+            ->line('Caso não tenha sido você que solicitou a alteração de senha, ignore este e-mail')
+            ->salutation('Até breve!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
